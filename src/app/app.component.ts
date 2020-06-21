ae374adf3f973f80d13f147df57adb9fb451509e
Marcos Lozina <marcoslozina@gmail.com>
2020-06-21 00:03:25 -0300

    upload example project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CookieService } from 'ngx-cookie-service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'sampleCokieProject';
+export class AppComponent implements OnInit {
+  title = 'Lectura de cookies desde angular';
+  private cookieValue: string;
+
+    constructor(private cookieService:CookieService) {}
+
+    public ngOnInit(){
+      this.cookieService.set('cookieName','cookieValue1');
+      this.cookieService.set('cookieName','cookieValue2');
+      this.cookieService.set('cookieName','cookieValue3');
+      this.cookieValue = this.cookieService.get('cookieName');
+      console.log('Valor leido de la cookie: '+  this.cookieValue);
+    }
+
+
 }
